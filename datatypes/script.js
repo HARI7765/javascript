da3d76a9c11ec44b4c10e1765e8b9b3cec67eb0b
HARI7765 <nharikrishnanm@gmail.com>
2025-03-03 16:22:28 +0530

    updated

---
+++ datatypes/script.js
@@ -0,0 +1,27 @@
+let a='hari'
+console.log(a)
+console.log(typeof a)
+
+let b=10
+console.log(b)
+console.log(typeof b)
+
+let c=10.5
+console.log(c)
+console.log(typeof c)
+
+let d=true
+console.log(d)
+console.log(typeof d)
+
+let e=null
+console.log(e)
+console.log(typeof e)
+
+let f = undefined
+console.log(f)
+console.log(typeof f)
+
+let g = BigInt(10)
+console.log(g)
+console.log(typeof g)

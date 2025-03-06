bf1b1aa0c9e0143d1d722e1a643722a354f1e0c6
HARI7765 <nharikrishnanm@gmail.com>
2025-03-06 15:47:04 +0530

    updated

---
+++ loop/script.js
@@ -0,0 +1,57 @@
+// let i=1
+// while (i<=10) {
+//   alert(i)
+//   console.log(i)
+//   i++
+// }
+
+// let i=1
+// do {
+//     alert(i)
+//     console.log(i)
+//     i++
+// } 
+// while (i<=10)
+
+// for (let i=1; i<=10; i++) {
+//     alert(i)
+//     console.log(i)
+// }
+// let l=[10,20,30,40,50]
+// for (i in l) {
+//     console.log(l[i])
+// }
+// for (i of l) {
+//     console.log(i)
+//     i++
+// }
+// while (i<=10) 
+
+// for (let i=1; i<=10; i++) {
+// alert(i)
+// console.log(i)
+// }
+
+// let l=[10,20,30,40,50]
+// for (i in l) {
+// console.log(l[i])
+// }
+// for (i of l) {
+// console.log(i)
+// }
+
+// let d = {name:"akhil",age:25}
+// for (i in d) {
+// console.log(d[i])
+// }
+// for (i of d) {
+//     console.log(i)
+// }
+
+
+let a = parseInt(prompt("Enter a number"))
+let b = parseInt(prompt("Enter a number"))
+console.log(a),console.log(typeof a)
+console.log(b),console.log(typeof b)
+alert(a),alert(typeof a)
+alert(b),alert(typeof b)
---
+++ multiplication/script.js
@@ -0,0 +1,8 @@
+let i = parseInt(prompt("Enter a number"))
+let a = parseInt(prompt("Enter a number"))
+while (a<=10) {
+    console.log(i*a)
+    a++
+}
+
+    

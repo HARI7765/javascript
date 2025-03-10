a88b2dc6844c8740c3c266a647f8c72bcad8b230
HARI7765 <nharikrishnanm@gmail.com>
2025-03-10 15:14:56 +0530

    updated

---
+++ string/script.js
@@ -0,0 +1,22 @@
+let a = "welcome"
+console.log(a[0])
+console.log(a[1])
+console.log(a.length)
+console.log(a.charAt(3))
+console.log(a.concat('hello'))
+console.log(a.endswith('e'))
+console.log(a.padend(20),'h1')
+console.log(a.padstart(20),)
+
+
+let b = "        hello        "
+console.log(b.trim())
+console.log(b.trimstart())
+console.log(b.trimend())
+
+console.log(a.slice(0,4))
+console.log(a.slice(1,4))
+
+console.log(a.split('c'))
+console.log(a.tolowercase())
+console.log(a.touppercase())

a08cce6217ed85f905201214f14d7688410c0f0c
HARI7765 <nharikrishnanm@gmail.com>
2025-03-11 16:33:53 +0530

    updated

--- work/reverse of a string/script.js
+++ work/reverse of a string/script.js
@@ -1,6 +1,21 @@
-function reverse(){
-    let a = document.getElementById("input")
-    let b = a.value.split("").reverse().join("")
-    document.getElementById("result").innerHTML = b
+// function reverse(){
+//     let a = document.getElementById("input")
+//     let b = a.value.split("").reverse().join("")
+//     document.getElementById("result").innerHTML = b
+// }
+
+
+// using for loop
+
+
+function reverse() {
+    let inputText = document.getElementById("input").value;
+    let reversedText = "";
+
+    for (let i = inputText.length - 1; i >= 0; i--) {
+        reversedText += inputText[i];
+    }
+
+    document.getElementById("result").textContent = reversedText;
 }
 

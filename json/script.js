fe697ae92efc54f9798a1adea3a2ed65e176c4f9
HARI7765 <nharikrishnanm@gmail.com>
2025-03-14 15:50:14 +0530

    updated

---
+++ json/script.js
@@ -0,0 +1,15 @@
+// const jsonstr = '[{"name":"John","age":30,}]'
+// console.log(jsonstr)
+// console.log(typeof jsonstr)
+// const obj = JSON.parse(jsonstr)
+// console.log(obj[0])
+
+const a = [{name:'manu',age:25}]
+const jsonstr1 = JSON.stringify(a)
+console.log(jsonstr1)
+
+
+
+a = 10
+b = 10
+console.log(a,b)
